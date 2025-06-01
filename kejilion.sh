027c36a3f3ae3cb604d9a7a1e69be08747111cd6
zaixiangjian <zaihuigengmei@outlook.com>
2025-06-01 21:50:53 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -6575,7 +6575,7 @@ EOF
 
 			docker_name="caddy"
 			docker_img="caddy"
-			docker_port="80/443"
+			docker_port="8080/8443"
 			docker_describe="Caddy 自动 HTTPS 反向代理服务器"
 			docker_url="官网：https://caddyserver.com/"
 			docker_use="echo -e '\033[32m访问地址：https://$caddy_domain\033[0m\n反代目标：$proxy_target'"
