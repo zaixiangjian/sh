7af760def4c696463f8e128d4e7897e816de5a53
zaixiangjian <zaihuigengmei@outlook.com>
2025-07-14 00:07:47 +0800

    Update mimabeifen.sh

--- mimabeifen.sh
+++ mimabeifen.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Create a tar archive of the web directory
-cd /home/web/ && tar czvf mima_$(date +"%Y%m%d%H%M%S").tar.gz vaultwarden
+cd /home/web/ && tar czvf /home/密码/mima_$(date +"%Y%m%d%H%M%S").tar.gz vaultwarden
+
 
 # Transfer the tar archive to another VPS
-cd /home/ && ls -t /home/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/
+cd /home/密码 && ls -t *.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 "{}" root@vpsip:/home/密码/
 
 # Keep only 5 tar archives and delete the rest
-cd /home/ && ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
+cd /home/ && ls -t /home/*.tar.gz | tail -n +10 | xargs -I {} rm {}
