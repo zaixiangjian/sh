f707257f5c7e6434cec9629359c0c59d1aac15b6
zaixiangjian <zaihuigengmei@outlook.com>
2025-07-14 21:21:11 +0800

    Update zidongbeifen.sh

--- zidongbeifen.sh
+++ zidongbeifen.sh
@@ -322,7 +322,7 @@ EOF
 
           rm -f "$TMP_SCRIPT" "$OBFUSCATED_SCRIPT" luntanbeifen.sh
 
-          read -e -p "每几天备份一次（例如3）: " interval
+          read -e -p "每几天备份一次（例如3天）: " interval
           read -e -p "每天几点传送（0-23）: " chuan_hour
           read -e -p "每天几分传送（0-59）: " chuan_min
 
