c7ccc9cc087b26a7e4ef4f597119b6d6b34cecfd
zaixiangjian <zaihuigengmei@outlook.com>
2025-07-17 20:08:15 +0800

    Update beifen.sh

--- beifen.sh
+++ beifen.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-# Create a tar archive of the web directory
-cd /home/ && tar czvf web_$(date +"%Y%m%d%H%M%S").tar.gz web
+# Create the backup directory if it doesn't exist
+mkdir -p /home/博客
+
+# Create a tar archive of the web directory and save it in /home/博客/
+tar czvf /home/博客/web_$(date +"%Y%m%d%H%M%S").tar.gz -C /home/ web
+
+# Transfer the latest tar archive to another VPS
+# 如果你确认这是你自己的服务器，并且知道密钥变化是正常的，可以这样处理：
+# SSH密码变化ssh链接输入代码
+
+
+# ssh-keygen -f "/root/.ssh/known_hosts" -R "103.234.53.1"
+
+
 
 # Transfer the tar archive to another VPS
-cd /home/ && ls -t /home/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/
+ls -t /home/博客/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/博客
 
-# Keep only 5 tar archives and delete the rest
-cd /home/ && ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
+
+# Keep only 5 tar archives in /home/博客/ and delete the rest
+cd /home/博客/ && ls -t *.tar.gz | tail -n +4 | xargs -I {} rm {}
