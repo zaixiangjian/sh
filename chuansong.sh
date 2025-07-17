2912dffb3e397051540eecb9c18b0879a65d520c
zaixiangjian <zaihuigengmei@outlook.com>
2025-07-17 20:09:57 +0800

    Update chuansong.sh

--- chuansong.sh
+++ chuansong.sh
@@ -2,7 +2,7 @@
 
 
 # Transfer the tar archive to another VPS
-cd /home/ && ls -t /home/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/
+ls -t /home/博客/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/博客
 
 # Keep only 5 tar archives and delete the rest
-cd /home/ && ls -t /home/*.tar.gz | tail -n +4 | xargs -I {} rm {}
+cd /home/博客/ && ls -t /home/博客/*.tar.gz | tail -n +4 | xargs -I {} rm {}
