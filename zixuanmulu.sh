5e0b94a0314e2cce2a162083f422ee10d5be4e08
zaixiangjian <zaihuigengmei@outlook.com>
2025-08-29 22:48:13 +0800

    Update zixuanmulu.sh

--- zixuanmulu.sh
+++ zixuanmulu.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# 本地目录，支持手动输入，如果不输入使用默认值
-read -e -p "请输入要传送的本地目录（默认 /home/docker/wangpan/）: " input_dir
-SRC="${input_dir:-/home/docker/wangpan/}"
+# 手动输入本地目录
+read -e -p "请输入要传送的本地目录（如 /home/docker/wangpan）: " SRC
 
 # 如果末尾没有 / 自动添加
 [[ "${SRC: -1}" != "/" ]] && SRC="$SRC/"
 
 # 远程服务器目录，与本地一致
-DEST="root@vpsip:$SRC"
+read -e -p "请输入远程服务器IP: " REMOTE_IP
+DEST="root@$REMOTE_IP:$SRC"
 
-# 远程服务器密码（如果使用密码方式）
-PASS="vps密码"
+# 远程服务器密码
+read -s -p "请输入远程服务器密码: " PASS
+echo
 
 # 创建远程目录（如果不存在）
-sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no root@vpsip "mkdir -p $SRC"
+sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no root@$REMOTE_IP "mkdir -p $SRC"
 
 # 使用 sshpass 调用 rsync
 echo "开始同步本地文件夹到远程服务器..."
