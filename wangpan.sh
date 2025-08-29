1b4df1b01f913a702b3b4e7a4e9ad087c3c6e75d
zaixiangjian <zaihuigengmei@outlook.com>
2025-08-29 22:02:20 +0800

    Update wangpan.sh

--- wangpan.sh
+++ wangpan.sh
@@ -2,7 +2,7 @@
 
 # 本地目录
 SRC="/home/docker/wangpan/"
-# 远程服务器目录
+# 远程服务器目录，与本地一致
 DEST="root@vpsip:$SRC"
 
 # 远程服务器密码
@@ -11,7 +11,7 @@ PASS="vps密码"
 # 创建远程目录（如果不存在）
 sshpass -p "$PASS" ssh -o StrictHostKeyChecking=no root@vpsip "mkdir -p $SRC"
 
-# 使用 sshpass + rsync 同步文件夹
+# 使用 sshpass 调用 rsync
 echo "开始同步本地文件夹到远程服务器..."
 sshpass -p "$PASS" rsync -avz --delete -e "ssh -o StrictHostKeyChecking=no" "$SRC/" "$DEST/"
 
