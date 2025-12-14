928af7944445a7d2ec517b9b2f4c90a0c67ead02
zaixiangjian <zaihuigengmei@outlook.com>
2025-12-14 17:59:14 +0800

    Create waibucunchu.sh

---
+++ waibucunchu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create the backup directory if it doesn't exist
+mkdir -p /home/网盘/
+
+# Create a tar archive of the web directory and save it in /home/beifen/
+tar -czvf /home/网盘/wangpan_$(date +%Y%m%d%H%M%S).tar.gz -C /home/docker wangpan
+
+# Transfer the latest tar archive to another VPS
+# 如果你确认这是你自己的服务器，并且知道密钥变化是正常的，可以这样处理：
+# SSH密码变化ssh链接输入代码
+
+
+# ssh-keygen -f "/root/.ssh/known_hosts" -R "103.234.53.1"
+
+#                                                                                         别忘记哈希加密
+ls -t /home/网盘/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/网盘
+
+
+
+# Keep only 5 tar archives in /home/beifen/ and delete the rest
+cd /home/密码/ && ls -t *.tar.gz | tail -n +10 | xargs -I {} rm {}
+cd /home/网盘/ && ls -t *.tar.gz | tail -n +10 | xargs -I {} rm {}
+
+cd /home/博客/ && ls -t *.tar.gz | tail -n +4 | xargs -I {} rm {}
+cd /home/论坛/ && ls -t *.tar.gz | tail -n +4 | xargs -I {} rm {}
+cd /home/ && ls -t *.tar.gz | tail -n +4 | xargs -I {} rm {}
