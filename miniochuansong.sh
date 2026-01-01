cc64dc999abec9c3e223b09d6869aab431a12d13
zaixiangjian <zaihuigengmei@outlook.com>
2026-01-02 06:34:41 +0800

    Update password and IP in miniochuansong.sh

--- miniochuansong.sh
+++ miniochuansong.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # 传输整个 minio 目录（rsync 比 scp 更稳）
-sshpass -p "$usepasswd" rsync -av -e "ssh -p 22 -o StrictHostKeyChecking=no" /home/docker/minio/ root@"$useip":/home/docker/minio/
+sshpass -p "vps密码" rsync -av -e "ssh -p 22 -o StrictHostKeyChecking=no" /home/docker/minio/ root@"vpsip":/home/docker/minio/
