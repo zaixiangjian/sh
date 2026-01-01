e22d5b758d8a5795e86d5a386c848e787f1427c4
zaixiangjian <zaihuigengmei@outlook.com>
2026-01-02 07:28:13 +0800

    Update miniochuansong.sh

--- miniochuansong.sh
+++ miniochuansong.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# 传输整个 minio 目录（rsync 比 scp 更稳）
-sshpass -p "vps密码" rsync -av -e "ssh -p 22 -o StrictHostKeyChecking=no" /home/docker/minio/ root@"vpsip":/home/docker/minio/
+# 传输整个 minio 目录（本地删远程也删）
+sshpass -p "vps密码" rsync -av --delete -e "ssh -p 22 -o StrictHostKeyChecking=no" /home/docker/minio/ root@"vpsip":/home/docker/minio/
