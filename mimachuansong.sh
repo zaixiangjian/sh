02e3cacedde61c1b2842fdc3bec244da71721957
zaixiangjian <zaihuigengmei@outlook.com>
2026-01-06 19:18:30 +0800

    Update mimachuansong.sh

--- mimachuansong.sh
+++ mimachuansong.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Transfer the tar archive to another VPS
-ls -t /home/密码/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} ailiaobiji@vpsip:/home/密码
+ls -t /home/密码/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} ailiaobiji@vpsip:/mima
