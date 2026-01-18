db9f3a1f067b1329a0c1408ddc059a7aa4aa7348
zaixiangjian <zaihuigengmei@outlook.com>
2026-01-19 05:38:48 +0800

    Update mimachuansong2.sh

--- mimachuansong2.sh
+++ mimachuansong2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Transfer the tar archive to another VPS
-ls -t /home/web/密码/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} zaihuigengmei@vpsip:/home/zaihuigengmei/mima_backup
+ls -t /home/web/密码/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} zaihuigengmei@vpsip:/zaihuigengmei/mima_backup
