29c193438abadbf93fcbebb00c70142a3942c0c7
zaixiangjian <zaihuigengmei@outlook.com>
2026-01-19 03:49:03 +0800

    Update mimabeifen.sh

--- mimabeifen.sh
+++ mimabeifen.sh
@@ -28,10 +28,10 @@ trap 'rm -f "$LOCKFILE" "$PIDFILE"' EXIT
 
 
 # Create the backup directory if it doesn't exist
-mkdir -p /home/密码/
+mkdir -p /home/web/密码/
 
 # Create a tar archive of the web directory and save it in /home/beifen/
-tar -czvf /home/密码/mima_$(date +%Y%m%d%H%M%S).tar.gz -C /home/docker vaultwarden
+tar -czvf /home/web/密码/mima_$(date +%Y%m%d%H%M%S).tar.gz -C /home/web/ vaultwarden
 
 # Transfer the latest tar archive to another VPS
 # 如果你确认这是你自己的服务器，并且知道密钥变化是正常的，可以这样处理：
@@ -41,13 +41,9 @@ tar -czvf /home/密码/mima_$(date +%Y%m%d%H%M%S).tar.gz -C /home/docker vaultwa
 # ssh-keygen -f "/root/.ssh/known_hosts" -R "103.234.53.1"
 
 #                                                                                         别忘记哈希加密
-ls -t /home/密码/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/密码
-
-
+ls -t /home/web/密码/*.tar.gz | head -1 | xargs -I {} sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 {} root@vpsip:/home/密码
 
 # Keep only 5 tar archives in /home/beifen/ and delete the rest
-cd /home/密码/ && ls -t *.tar.gz | tail -n +10 | xargs -I {} rm {}
-
+cd /home/web/密码/ && ls -t *.tar.gz | tail -n +10 | xargs -I {} rm {}
 cd /home/博客/ && ls -t *.tar.gz | tail -n +4 | xargs -I {} rm {}
-cd /home/论坛/ && ls -t *.tar.gz | tail -n +4 | xargs -I {} rm {}
-cd /home/ && ls -t *.tar.gz | tail -n +4 | xargs -I {} rm {}
+
