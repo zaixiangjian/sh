d7cf3bd3dbc168f5e22a5d2347d1d7852f743634
zaixiangjian <zaihuigengmei@outlook.com>
2026-01-19 05:52:17 +0800

    Update mimachuansong2.sh

--- mimachuansong2.sh
+++ mimachuansong2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 本地备份目录（保留中文）
-LOCAL_DIR="/home/密码"
+LOCAL_DIR="/home/web/密码"
 
 # 远程 VPS 信息
 REMOTE_USER="ailiaobiji"
@@ -10,7 +10,7 @@ REMOTE_PASS="vps密码"
 REMOTE_DIR="~/mima_backup"  # 远程目录（放在远程用户 home 下）
 
 # 获取最新的 tar.gz 文件
-LATEST_FILE=$(ls -t "/home/密码/"*.tar.gz 2>/dev/null | head -1)
+LATEST_FILE=$(ls -t "/home/web/密码/"*.tar.gz 2>/dev/null | head -1)
 
 if [ -n "$LATEST_FILE" ]; then
     sshpass -p 'vps密码' scp -o StrictHostKeyChecking=no -P 22 "$LATEST_FILE" ailiaobiji@vpsip:~/mima_backup/
