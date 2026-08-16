6bf2371c7b8ae41b2534b66210a40c2171ff343c
zaixiangjian <zaihuigengmei@outlook.com>
2026-08-16 19:16:51 +0800

    Update kejilion.sh

--- kejilion.sh
+++ kejilion.sh
@@ -6957,7 +6957,7 @@ linux_panel() {
     echo -e "${gl_kjlan}97.  ${gl_bai}IP白名单模式                           ${gl_kjlan}98.  ${gl_bai}安装Google${gl_huang}"
     echo -e "${gl_kjlan}------------------------"
     echo -e "${gl_kjlan}99.  ${gl_bai}Hermes机器人爱马仕                 ${gl_kjlan}100. ${gl_bai}caddy官方docker安装"
-    echo -e "${gl_kjlan}101.  ${gl_bai}agent-ai备份"
+    echo -e "${gl_kjlan}101.  ${gl_bai}agent-ai备份                 ${gl_kjlan}102. ${gl_bai}lobehub安装webai"
     echo -e "${gl_kjlan}------------------------"
     echo -e "${gl_kjlan}990.  ${gl_bai}安装的应用以及应用端口"
     echo -e "${gl_kjlan}996.  ${gl_bai}CDN安装 ${gl_huang}★${gl_bai}                           ${gl_kjlan}997.  ${gl_bai}PVE开小鸡面板"
@@ -12677,6 +12677,12 @@ EOF
         echo "✅ agent-ai备份安装成功..."
         ;;
 
+      102)
+        clear
+        echo "▶️ 安装lobehub网页版本..."
+		curl -fsSL https://raw.githubusercontent.com/zaixiangjian/sh/refs/heads/main/ai/lobehub.sh -o /root/lobehub.sh && chmod +x /root/lobehub.sh && /root/lobehub.sh
+        echo "✅ lobehub安装成功..."
+        ;;
 
 
 
